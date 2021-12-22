5087eaa814e83a4e7dc1ca82f429447e5fdeaef2
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-12-22 12:43:01 +0100

    Add pop up for the comment section

--- src/index.js
+++ src/index.js
@@ -46,25 +46,42 @@ document.querySelectorAll('i').forEach((like) => {
       getShows()
 	})
 })
+
+document.querySelectorAll('.cmntBtn-button').forEach((item) => {
+  item.addEventListener('click', () => {
+    const showComment = document.querySelector('.commentPopUp');
+    console.log('Clicked');
+    showComment.innerHTML = `
+    <div class="popup">
+        <div class="popup-info">
+            <img src="https://static.remove.bg/remove-bg-web/6cc620ebfb5922c21227f533a09d892abd65defa/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png" alt="">
+        </div>
+        <p>Name</p>
+        <div>
+            <p>something</p>
+            <p>something</p>
+            <p>something</p>
+            <p>something</p>
+        </div>
+        <h5>comments(count)</h5>
+        <form action="">
+            <input type="text">
+            <input type="text">
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+`
+showComment.style.display = 'block';
+    
+  });
+});
 }
 
 async function count(obj) {
-	const response = await fetch(countUrl, {method: 'POST',headers: {'Content-Type':'application/json'},body:JSON.stringify(obj)});
+	const response = await fetch(countUrl, 
+    {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body:JSON.stringify(obj)});
     const countData = await response.json();
-}
-
-
-const comment = document.querySelector('.reBtn')
-comment.addEventListener('click', () => {
-  console.log('clicked')
-  // comment.innerHTML = `
-  // <div>Test</div>
-  // `
-})
-
-
-
-
-
-	
-
+};
