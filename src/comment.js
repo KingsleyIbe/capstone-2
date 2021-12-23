cc05afe5c9090a2075c383b55843584e1b6aaf0e
Mithlesh kumar <mithlesh.9m@gmail.com>
2021-12-23 18:10:34 +0530

    Update comment.js
    Update getcomments function

--- src/comment.js
+++ src/comment.js
@@ -1,28 +1,20 @@
+const commentURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Q4ShCASnnzP55bg7hv5u/comments';
 
-const commentId = 'Q4ShCASnnzP55bg7hv5u';
-const commentURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
-
-
-const postComments = async (item_id, username, comment) => {
-    const data = await fetch(`${commentURL}/${commentId}/comments/`, {
+const postComments = async (obj) => {
+    await fetch(commentURL, {
       method: 'POST',
       cache: 'no-cache',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ item_id, username, comment}),
+      body: JSON.stringify(obj),
     })
-    console.log(data);
   };
   
   const getComments = async (id) => {
-    const response = await fetch(`${commentURL}/${commentId}/comments/?item_id=id`);
+    const response = await fetch(commentURL+'?item_id='+ id);
     const resData = await response.json();
-    const data = await resData.result;
-    const obj = {'item_id': item_id};
-    id = Number(item.id.split("-")[1]);
-    // return data;
-    console.log(data);
+    return resData;
   };
 
   export { postComments, getComments };
